9bedcee84e4b75b9be08695d0e4423527bb6ddd2
andreyks95 <andreyks95@gmail.com>
2017-02-22 12:00:23 +0200

    Сделана 3 лаба по ТРСПО)

--- Client/Client.cs
+++ Client/Client.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using System.Net.Sockets;
+using System.Numerics;
 using System.Text;
 
 namespace TcpClientApp
@@ -11,29 +15,44 @@ namespace TcpClientApp
 
         static void Main(string[] args)
         {
+            Stopwatch sw = new Stopwatch();
             try
             {
                 TcpClient client = new TcpClient();
                 client.Connect(server, port);
-
-                byte[] data = new byte[256];
-                StringBuilder response = new StringBuilder();
                 NetworkStream stream = client.GetStream();
 
-                //чтение записи
-                do
-                {
-                    int bytes = stream.Read(data, 0, data.Length);
-                    response.Append(Encoding.UTF8.GetString(data, 0, bytes));
-                }
-                while (stream.DataAvailable); // пока данные есть в потоке
+                //получаем сообщение из сервера
+                string messageGet = GetMessage(stream);
+                //разбиваем для оси X и Y 
+                string[] message = GetStringData(messageGet, new char[] { '\n' });
+                //создаём массивы значений отдельно для оси X и Y
+                string[] messageX = GetStringData(message[0], new char[] { 'X', 'Y', ':', ' ', '\n' }),
+                    messageY = GetStringData(message[1], new char[] { 'X', 'Y', ':', ' ', '\n' });
 
-                Console.WriteLine(response.ToString());
+                List<string> axisX = GetStringTrim(messageX),
+                             axisY = GetStringTrim(messageY);
+                //конвертируем значение строк в числа
+                double startX = Convert.ToDouble(axisX[0]),
+                    endX   = Convert.ToDouble(axisX[1]),
+                    stepX  = Convert.ToDouble(axisX[2]),
+                    startY = Convert.ToDouble(axisY[0]),
+                    endY   = Convert.ToDouble(axisY[1]),
+                    stepY  = Convert.ToDouble(axisY[2]);
 
-                //отправка записи
-                string message = "Данные я принял, можешь не париться)";
-                data = Encoding.Unicode.GetBytes(message);
-                stream.Write(data, 0, data.Length);
+                Console.WriteLine("Ось X: начало промежутка = {0}; конец = {1}; шаг = {2}", startX, endX, stepX);
+                Console.WriteLine("Ось Y: начало промежутка = {0}; конец = {1}; шаг = {2}", startY, endY, stepY);
+
+                sw.Start();
+                List<double> maxResultsList = GetValuesXY(startX, endX, stepX, startY, endY, stepY); //получили все максимумы на оси XY
+                double max = GetMax(maxResultsList); //Максимум с оси X
+                sw.Stop();
+                Console.WriteLine("Максимум на текущем отрезке: {0}", max);
+                Console.WriteLine("Длительность выполнения расчётов (сек.): " + sw.Elapsed.TotalSeconds);
+
+                //отправляем сообщение серверу
+                SendMessage(stream, max);
+                Console.WriteLine("Данные отправлены серверу на дальнейшую обработку");
 
                 // Закрываем потоки
                 stream.Close();
@@ -50,6 +69,109 @@ namespace TcpClientApp
 
             Console.WriteLine("Запрос завершен...");
             Console.Read();
+        }
+
+
+        private static string GetMessage(NetworkStream stream)
+        {
+            byte[] data = new byte[256];
+            StringBuilder getMessage = new StringBuilder();
+            //чтение записи
+            do
+            {
+                int bytes = stream.Read(data, 0, data.Length);
+                getMessage.Append(Encoding.UTF8.GetString(data, 0, bytes));
+            }
+            while (stream.DataAvailable); // пока данные есть в потоке
+            return getMessage.ToString();
+        }
+
+        private static void SendMessage(NetworkStream stream, double max)
+        {
+            byte[] data = new byte[256];
+            //отправка записи
+            string message = max.ToString();
+            data = Encoding.Unicode.GetBytes(message);
+            stream.Write(data, 0, data.Length);
+
+        }
+
+        private static string[] GetStringData(string message, char[] charsets)
+        {
+            string[] messageOne = message.Split(charsets);
+            return messageOne;
+        }
+
+       private static List<string> GetStringTrim(string[] message)
+        {
+            List <string> strList = new List<string>();
+            for (int i = 0; i < message.Length; i++)
+            {
+                if (message[i] != "")
+                    strList.Add(message[i]);
+            }
+            return strList;
+        }
+
+        //Вычисления
+        private static double GetMax(List<double> array)
+        {
+            return double.IsNaN(array[0]) ? 0 : array.Max();
+        }
+
+
+        private static List<double> GetValuesXY(double startX, double endX, double stepX, double startY, double endY, double stepY)
+        {
+            //Вместо Parellel.For, потому что нельзя указать свой шаг
+            //Задействует все ядра процессора распиливание задачи
+            //SteppedIterator (Enumerable<double>) перечислитель для дипазона, 
+            //возвращает текущее значение в диапазоне Х здесь как index
+
+            var valuesYX = new List<double>();
+            object localLockObject = new object();
+
+            System.Threading.Tasks.Parallel.ForEach(
+               SteppedIterator((double)startX, (double)endX, stepX),
+               () => new List<double>(),
+               (index, state, localList) =>
+               {
+                   //double max = GetMax(GetValuesY(startY, endY, stepY, index));
+                   //localList.Add(max);
+                   localList.Add(GetMax(GetValuesY(startY, endY, stepY, index)));
+                   if (localList.Count > 1000)
+                   {
+                       //max = GetMax(localList);
+                       double max = GetMax(localList);
+                       localList.Clear();
+                       localList.TrimExcess();
+                       localList.Add(max);
+                   }
+                   return localList; //с каждой итерацией увеличиваем localList, закидываем текущий диапазон X и считаем его на промежутке
+               },
+               (finalResult) => { lock (localLockObject) valuesYX.AddRange(finalResult); }
+               );
+            return valuesYX;
+        }
+
+        private static IEnumerable<double> SteppedIterator(double startIndex, double endIndex, double stepSize)
+        {
+            for (double i = startIndex; i < endIndex; i = i + stepSize)
+            {
+                yield return i;
+            }
+        }
+
+        private static List<double> GetValuesY(double startY, double endY, double stepY, double valueX)
+        {
+            List<double> valuesY = new List<double>();
+            for (double y = startY; y <= endY; y += stepY)
+                valuesY.Add(GetValueFunc(valueX, y));
+            return valuesY;
+        }
+
+        private static double GetValueFunc(double x, double y)
+        {
+            return x * x + 2 * y;
         }
     }
 }
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,9 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
+using System.Numerics;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace TcpListenerApp
@@ -13,6 +18,7 @@ namespace TcpListenerApp
         const int port = 8888; // порт для прослушивания подключений
         static void Main(string[] args)
         {
+            Stopwatch sw = new Stopwatch();
             //Список клиентов клиентов
             List<TcpClient> clients = new List<TcpClient>();
             List<NetworkStream> streams = new List<NetworkStream>();
@@ -41,155 +47,58 @@ namespace TcpListenerApp
                     else
                         break;
                 }
-                int response = 0;
+
+                //Теперь вводим диапазоны
+                Console.WriteLine(
+               "Поиск наибольшего значения функции нескольких переменных методом сканирования с заданным шагом");
+                int startX, endX, startY, endY;
+                double stepX, stepY;
+                List<double> arrayValues = new List<double>();
+                //считываем значения
+                Console.WriteLine("Введите: нач. значение, кон. значение и шаг для оси X: ");
+                startX = Convert.ToInt32(Console.ReadLine().Trim());
+                endX = Convert.ToInt32(Console.ReadLine().Trim());
+                string valueDoubleX = Console.ReadLine().Trim();
+                Console.WriteLine("Введите: нач. значение, кон. значение и шаг для оси Y: ");
+                startY = Convert.ToInt32(Console.ReadLine().Trim());
+                endY = Convert.ToInt32(Console.ReadLine().Trim());
+                string valueDoubleY = Console.ReadLine().Trim();
+                double.TryParse(valueDoubleX, NumberStyles.Any, CultureInfo.InvariantCulture, out stepX);
+                double.TryParse(valueDoubleY, NumberStyles.Any, CultureInfo.InvariantCulture, out stepY);
+
+                //посчитать процент одного клиента от всех доступных нам клиентов
+                double percentage = TakePercentage(clients.Count());
+
+                //получаем шаг для каждого клиента "для раздачи"
+                //Например: -20 ... 20 Клиентов 4  - шаг для каждого клиента 10 
+                var axis = TakeRangeAxis(startX, endX, startY, endY, percentage);
+                double rangeX = axis.Item1,
+                    rangeY = axis.Item2;
+
+                //с откуда начинать
+                double internalX = startX, 
+                       internalY = startY;
+               
+                List<double> maxList = new List<double>();
+                sw.Start();
                 //работаем с каждым клиентом
-                //сделать для каждого свой поток
+                //создаём для каждого свой поток
                 foreach (var currentClient in clients)
                 {
-                    response += 1;
-                    Task task = Task.Factory.StartNew(() => Message(currentClient, response));
+                    //отсылаем сначала промежуток для текущего клиента
+                    var task = Task.Factory.StartNew(() => Message(currentClient, internalX, internalX+rangeX, stepX, 
+                                                                                   internalY, internalY+rangeY, stepY));
                     task.Wait();
+                    //добавляем максимумы с каждого клиента
+                    maxList.Add(task.Result);
+                    //Увеличиваем интервалы для следующего клиента
+                    internalX += rangeX;
+                    internalY += rangeY;
                 }
-
-                /*int response = 0;
-                //работаем с каждым клиентом
-                //сделать для каждого свой поток
-                foreach (var currentClient in clients)
-                {
-                    response += 1;
-                    Message(currentClient, response);
-                }*/
-
-                #region Черновик
-
-                #region старый цикл
-                /*
-                while (true)
-                {
-
-                    // получаем входящее подключение
-                    TcpClient client = server.AcceptTcpClient();
-
-                    clients.Add(client);
-                    Console.WriteLine("Подключено клиент. Количество клиентов " + clients.Count());
-                    // streams.Add(client.GetStream());
-                    //Console.WriteLine("Подключен клиент. Выполнение запроса... stop для остановки и отдачи задача");
-
-                    //Если клиент подключён, продолжать дальше подкючения 
-                    //всё тормозиться из-за чтения записи. 
-                    //проверить успешно ли подключён клиент
-                    //если да 
-                    //то продолжить подключение
-                    //или отключить подключение 
-                    if (Console.ReadLine().ToLower() != "stop") continue;
-                    else
-                    {
-                        break;
-                    }
-                    
-                    // получаем сетевой поток для чтения и записи
-                    //NetworkStream stream = client.GetStream();
-                    //
-                    // сообщение для отправки клиенту
-                    //string response = "Привет мир";
-                    // преобразуем сообщение в массив байтов
-                    //byte[] data = Encoding.UTF8.GetBytes(response);
-                    //
-                    // отправка сообщения
-                    //stream.Write(data, 0, data.Length);
-                    //Console.WriteLine("Отправлено сообщение: {0}", response);
-                    // закрываем подключение
-                    //client.Close();
-                }
-            */
-                #endregion
-
-                #region более менее
-                /*int response = 0;
-                //работаем с каждым клиентом
-                //сделать для каждого свой поток
-                foreach (var currentClient in clients)
-                {
-                    // получаем сетевой поток для чтения и записи
-                    NetworkStream stream = currentClient.GetStream();
-
-                    // сообщение для отправки клиенту
-                    response += 1;
-                    // преобразуем сообщение в массив байтов
-                    byte[] data = Encoding.UTF8.GetBytes(response.ToString());
-                    // отправка сообщения
-                    stream.Write(data, 0, data.Length);
-                    Console.WriteLine("Отправлено сообщение: {0}", response);
-
-                    //для принятия сообщений
-                    data = new byte[256];
-                    StringBuilder builder = new StringBuilder();
-                    int bytes = 0;
-                    do
-                    {
-                        bytes = stream.Read(data, 0, data.Length);
-                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
-                    }
-                    while (stream.DataAvailable);
-                    string message = builder.ToString();
-                    Console.WriteLine("Принято сообщения " + message);
-
-                    // закрываем подключение
-                    currentClient.Close();
-                }*/
-                #endregion
-
-                #region Ещё одна запись
-                /*
-                int response = 0;
-                //работаем с каждым клиентом
-                //сделать для каждого свой поток
-                foreach (var currentClient in clients)
-                {
-                    // получаем сетевой поток для чтения и записи
-                    NetworkStream stream = currentClient.GetStream();
-                    response += 1;
-                    //Отправляем сообщение
-                    SendMessage(stream, response);
-                    //Принимаем сообщение
-                    GetMessage(stream);
-                    // закрываем подключение
-                    currentClient.Close();
-                }*/
-                #endregion
-
-                #region Оригинальная запись
-
-                /*while (true)
-                {
-                    Console.WriteLine("Ожидание подключений... ");
-
-                    // получаем входящее подключение
-                    TcpClient client = server.AcceptTcpClient();
-
-                    Console.WriteLine("Подключен клиент. Выполнение запроса... stop для остановки и отдачи задача");
-                    if (Console.ReadLine().ToLower() == "stop")
-                    {
-
-                    }
-                    // получаем сетевой поток для чтения и записи
-                    NetworkStream stream = client.GetStream();
-
-                    // сообщение для отправки клиенту
-                    string response = "Привет мир";
-                    // преобразуем сообщение в массив байтов
-                    byte[] data = Encoding.UTF8.GetBytes(response);
-
-                    // отправка сообщения
-                    stream.Write(data, 0, data.Length);
-                    Console.WriteLine("Отправлено сообщение: {0}", response);
-                    // закрываем подключение
-                    client.Close();
-                }*/
-
-                #endregion
-
-                #endregion
+                double max = GetMax(maxList);
+                sw.Stop();
+                Console.WriteLine("Максимум функции: " + max);
+                Console.WriteLine("Длительность выполнения расчётов (сек.): " + sw.Elapsed.TotalSeconds);
             }
             catch (Exception e)
             {
@@ -203,19 +112,23 @@ namespace TcpListenerApp
             }
         }
 
-        private static void Message(TcpClient currentClient, int response)
+        private static double Message(TcpClient currentClient, double startX, double endX, double stepX, double startY, double endY, double stepY)
         {
             // получаем сетевой поток для чтения и записи
             NetworkStream stream = currentClient.GetStream();
             //Отправляем сообщение
-            SendMessage(stream, response);
+            SendMessage(stream, startX, endX, stepX, startY, endY, stepY);
+            
             //Принимаем сообщение
-            GetMessage(stream);
-            // закрываем подключение
+            double result =  GetMessage(stream);
+
+            //закрываем подключение
             currentClient.Close();
+
+            return result;
         }
 
-        private static void GetMessage(NetworkStream stream)
+        private static double GetMessage(NetworkStream stream)
         {
 
             //для принятия сообщений
@@ -229,17 +142,42 @@ namespace TcpListenerApp
             }
             while (stream.DataAvailable);
             string message = builder.ToString();
-            Console.WriteLine("Принято сообщения " + message);
+            Console.WriteLine("Принято сообщение " + message + "\n");
+            return Convert.ToDouble(message);
         }
 
-        private static void SendMessage(NetworkStream stream, int response)
+        private static void SendMessage(NetworkStream stream, double startX, double endX, double stepX, double startY, double endY, double stepY)
         {
+            //отправляем данные клиенту о промежутке для X и Y в виде строки
+            string message = "X: " + startX + " " + endX + " " + stepX + "\nY: " + startY + " " + endY + " " + stepY;
             // сообщение для отправки клиенту
             // преобразуем сообщение в массив байтов
-            byte[] data = Encoding.UTF8.GetBytes(response.ToString());
+            byte[] data = Encoding.UTF8.GetBytes(message);
             // отправка сообщения
             stream.Write(data, 0, data.Length);
-            Console.WriteLine("Отправлено сообщение: {0}", response);
+            Console.WriteLine("\nОтправлено сообщение: \n{0}", message);
+        }
+
+
+        //Пример: X на промежутке -20 ... 20 => [-20] + [20] = 40 * 0.25 = 10 возвращаем этот диапазон для одного клиента
+        //аналогично для Y. Потом "засовываем" в кортеж и возвращаем
+        private static Tuple<double,double> TakeRangeAxis(double startX, double endX, double startY, double endY,  double percentage)
+        {
+            double rangeX = (Math.Abs(startX) + Math.Abs(endX)) * percentage,
+            rangeY = (Math.Abs(startY) + Math.Abs(endY)) * percentage;
+            return new Tuple<double, double>(rangeX, rangeY);
+        }
+
+        //Пример: 4 клиента, значит доля один клиент занимает 25% 
+        private static double TakePercentage(int countClients)
+        {
+           return (double) 1 / countClients;
+        }
+
+        //Поиск маскимума
+        private static double GetMax(List<double> array)
+        {
+            return double.IsNaN(array[0]) ? 0 : array.Max();
         }
     }
 }
